773d5e66f9cdf1aea5ff9db81eebb65d8a9c7cb6
butjar <martin.fleischer@bisdn.de>
2019-07-23 17:06:28 +0200

    [vlan] Move from QinQ to double C-tags
    The current hardeware filtering doesn't support QinQ tags (S-tag +
    C-tag). Move to double C-tagged interfaces.

--- accel-pppd/docker-entrypoint.sh
+++ accel-pppd/docker-entrypoint.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 set -e
 
-S_TAG=${S_TAG:-0}
-C_TAGS="$(echo {0..4094})"
+OUTER_TAG=${S_TAG:-0}
+INNER_TAGS="$(echo {0..4094})"
 IFACE=${IFACE:-eth0}
 CORES="$(grep -c ^processor /proc/cpuinfo)"
 
-create_s_tag () {
+create_vlan_interface () {
     iface=$1
-    s_tag=$2
-    s_iface=$iface.$s_tag
-    ip link add link $iface $s_iface type vlan proto 802.1ad id $s_tag
-    ip link set $s_iface up
-    echo "$s_iface"
+    tag=$2
+    proto=${3:-802.1Q}
+    vlan_iface=$iface.$tag
+    ip link add link $iface $vlan_iface type vlan proto proto id $tag
+    ip link set $vlan_iface up
+    echo "$vlan_iface"
 }
 
-create_c_tag () {
-    iface=$1
-    c_tag=$2
-    c_iface=$iface.$c_tag
-    ip link add link $iface $c_iface type vlan proto 802.1Q id $c_tag
-    ip l set $c_iface up
-    echo "$c_iface"
-}
+# Create interface for outer C-tag
+OUTER_IFACE=$(create_vlan_interface $IFACE $OUTER_TAG)
 
-# Create interface for S-tag
-S_IFACE=$(create_s_tag $IFACE $S_TAG)
-
-# Create interfaces for C-tags
-export -f create_c_tag
-export S_IFACE
-printf %s\\n $C_TAGS | xargs -n 1 -P $CORES -I {} bash -c 'create_c_tag "$S_IFACE" "{}" &> /dev/null'
+# Create interfaces for inner C-tags
+export -f create_vlan_interface
+export OUTER_IFACE
+printf %s\\n $INNER_TAGS | xargs -n 1 -P $CORES -I {} bash -c 'create_vlan_interface "$OUTER_IFACE" "{}" &> /dev/null'
 
 # Run command from CMD
 exec "$@"
